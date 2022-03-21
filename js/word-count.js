7f9bd6d960cc6b329eeaccccaa5c90f2110d1ff5
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-03-21 20:22:07 +0800

    Add reading time

--- js/word-count.js
+++ js/word-count.js
@@ -1,21 +1,15 @@
 let textarea = document.getElementById("textarea");
-
 if (localStorage.getItem('text-value')) {
     textarea.value = localStorage.getItem('text-value');
 }
-
 const trackText = e => {
     let words = e.target.value.split(/\s/g);
-
     if (words[words.length - 1] === '') {
         words.pop();
     }
-
     document.getElementById('words').innerHTML = words.length;
     document.getElementById('chars').innerHTML = e.target.value.replace(/\s/g, '').length;
     document.getElementById('chars-whitespace').innerHTML = e.target.value.length;
-
     localStorage.setItem('text-value', e.target.value)
 }
-
 textarea.addEventListener('input', trackText);
