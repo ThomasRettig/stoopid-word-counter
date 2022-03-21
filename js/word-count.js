794c4f120ac0e418fb5ee6c65decf705f241fbb1
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-03-21 18:31:11 +0800

    Major overhaul

--- js/word-count.js
+++ js/word-count.js
@@ -1,10 +1,12 @@
 let textarea = document.getElementById("textarea");
-if (localStorage.getItem('TEXT_VALUE')) {
-    textarea.value = localStorage.getItem('TEXT_VALUE');
+
+if (localStorage.getItem('text-value')) {
+    textarea.value = localStorage.getItem('text-value');
 }
 
 const trackText = e => {
     let words = e.target.value.split(/\s/g);
+
     if (words[words.length - 1] === '') {
         words.pop();
     }
@@ -13,8 +15,7 @@ const trackText = e => {
     document.getElementById('chars').innerHTML = e.target.value.replace(/\s/g, '').length;
     document.getElementById('chars-whitespace').innerHTML = e.target.value.length;
 
-    localStorage.setItem('TEXT_VALUE', e.target.value)
+    localStorage.setItem('text-value', e.target.value)
 }
 
 textarea.addEventListener('input', trackText);
-
---
+++ sw.js
@@ -0,0 +1,31 @@
+var CACHE = 'cache-v1';
+
+var urls = [
+  '/',
+  'css/styles.css',
+  'icon-256x256.svg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(function(cache) {
+        return cache.addAll(urls);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
