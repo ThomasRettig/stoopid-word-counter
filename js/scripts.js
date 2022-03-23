93718200aea9895c61433ca9842163ee357387f5
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-03-23 10:10:33 +0800

    Update some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,23 @@ document.getElementById('toggleFont').addEventListener('change', function() {
     }
 });
 
+// font size slider
+document.getElementById('fontSlider').addEventListener('input', function () {
+  document.getElementById('textarea').style.fontSize = this.value + "px";
+});
+
+// make textarea resize height automatically
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
+
 /**
  * Calculate byte size of a text snippet
  * @author Lea Verou
