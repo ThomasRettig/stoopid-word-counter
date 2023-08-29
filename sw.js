1c7cc26b6c5da8d1b12da7409a378b0b5aa2ef81
Thomas Rettig <thomas.rettig.11@gmail.com>
2023-08-29 20:47:22 +0800

    Standardise formatting

--- sw.js
+++ sw.js
@@ -1,40 +1,37 @@
-var CACHE = 'cache-v1';
+var CACHE = "cache-v1";
 
 var urls = [
-  '/',
-  'js/scripts.js',
-  'css/styles.css',
-  'icon-256x256.svg',
-  'icon-256x256.png',
-  'install.svg',
-  'share.svg',
-  'settings.svg',
-  'sw.js',
-  'manifest.webmanifest',
-  'fonts/Inter-Bold.woff2',
-  'fonts/Inter-Regular.woff2'
+  "/",
+  "js/scripts.js",
+  "css/styles.css",
+  "icon-256x256.svg",
+  "icon-256x256.png",
+  "install.svg",
+  "share.svg",
+  "settings.svg",
+  "sw.js",
+  "manifest.webmanifest",
+  "fonts/Inter-Bold.woff2",
+  "fonts/Inter-Regular.woff2",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE)
-      .then(function(cache) {
-        return cache.addAll(urls);
-      })
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(urls);
+    }),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    }),
   );
 });
